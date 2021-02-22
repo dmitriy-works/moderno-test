ef69da865ca90ac929f67d0bfd9cd265b1799bcb
dmitriy-works <dmitriy-works@ukr.net>
2021-02-22 17:19:49 +0200

    added advantages

--- app/js/script.js
+++ app/js/script.js
@@ -6,5 +6,12 @@ $(function(){
     starWidth: "12px"
   });
 
+  $(".products-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+
   var mixer = mixitup('.products__inner-box');
 });
